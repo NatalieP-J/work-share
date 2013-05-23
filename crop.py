f88b61f5f158e86a03f5a7cf0438d961e90570a5
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2013-05-23 16:04:57 -0400

    Moar files.

--- crop.py
+++ crop.py
@@ -80,89 +80,18 @@ except TypeError:
 #Question 2
 
 #Randomizing x and y
-def Randomize (x,y):
-    output=[]
-    ran_x=np.random.normal(loc=x,scale=1e-10)
-    ran_y=np.random.normal(loc=y,scale=1e-4)
-    point=[ran_x,ran_y]
-    output.append(point)
-    return output
+ran_ra=np.random.normal(loc=ra,scale=1e-10)
+ran_dec=np.random.normal(loc=dec,scale=1e-4)
+
+print ran_ra
+print ran_dec
 
 
-points=np.array(Randomize(ra,dec))
-ran_ra=points[:,0]
-ran_ra=ran_ra[:,0]
-ran_ra=np.array(ran_ra)
-ran_dec=points[:,1]
-ran_dec=ran_dec[:,0]
-ran_dec=np.array(ran_dec)
 #Question 3
-
-def fitEllipse(x,y):
-    x = x[:,np.newaxis]
-    y = y[:,np.newaxis]
-    D =  np.hstack((x*x, x*y, y*y, x, y, np.ones_like(x)))
-    S = np.dot(D.T,D)
-    C = np.zeros([6,6])
-    C[0,2] = C[2,0] = 2; C[1,1] = -1
-    E, V =  eig(np.dot(inv(S), C))
-    n = np.argmax(np.abs(E))
-    a = V[:,n]
-    return a
-
-
-def ellipse_center(a):
-    b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
-    num = b*b-a*c
-    x0=(c*d-b*f)/num
-    y0=(a*f-b*d)/num
-    return np.array([x0,y0])
-
-def ellipse_angle_of_rotation( a ):
-    b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
-    return 0.5*np.arctan(2*b/(a-c))
-
-def ellipse_axis_length( a ):
-    b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
-    up = 2*(a*f*f+c*d*d+g*b*b-2*b*d*f-a*c*g)
-    down1=(b*b-a*c)*( (c-a)*np.sqrt(1+4*b*b/((a-c)*(a-c)))-(c+a))
-    down2=(b*b-a*c)*( (a-c)*np.sqrt(1+4*b*b/((a-c)*(a-c)))-(c+a))
-    res1=np.sqrt(up/down1)
-    res2=np.sqrt(up/down2)
-    return np.array([res1, res2])
-
-
-arc = 0.8
-R = np.arange(0,arc*np.pi, 0.01)
-x = ran_ra
-y = ran_dec
-
-
-a = fitEllipse(x,y)
-center = ellipse_center(a)
-phi = ellipse_angle_of_rotation(a)
-axes = ellipse_axis_length(a)
-
-print "center = ",  center
-print "angle of rotation = ",  phi
-print "axes = ", axes
-
-
-a, b = axes
-xx = center[0] + a*np.cos(R)*np.cos(phi) - b*np.sin(R)*np.sin(phi)
-yy = center[1] + a*np.cos(R)*np.sin(phi) + b*np.sin(R)*np.cos(phi)
-
-
-from pylab import *
-plot(x,y)
-plot(xx,yy, color = 'red')
-show()
-
-
 
 fig=plt.figure()
 ax=fig.add_subplot(111)
 ax.plot(ran_ra,ran_dec,'o',color='red')
 ax.plot(ra,dec,'o',color='blue')
 ax.plot()
-#plt.show()
+plt.show()
