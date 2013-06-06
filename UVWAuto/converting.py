9c42beb36bc24eb4875bb4e431f8a40d3a6eb16b
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2013-06-06 12:34:52 -0400

    Fixed for 1957+20 Output

--- UVWAuto/converting.py
+++ UVWAuto/converting.py
@@ -1,7 +1,17 @@
+import astropy.units as u
 from astropy.time import Time
 import manage as man
+import observability
+import numpy as np
 
-fname="Timestamp/timestamp_voltage.b1957+20.raw0.node0.scan0"
+def WriteFileCols(values1,values2,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values1)):
+            data.write("{0}\t{1}\n".format(values1[i],values2[i]))
+
+#File is formatted with spaces in between year, month, day etc. For some reason,# the seconds and anything less than seconds are specified separately
+
+fname="timestamp_voltage.b1957+20.raw0.node0.scan0"
 times=man.LoadData(fname)
 year=man.IterativeStrAppend(times,0)
 month=man.IterativeStrAppend(times,1)
@@ -24,10 +34,17 @@ for i in range(len(times)-2):
 t=Time(time, scale='utc')
 mjd=t.mjd
 
-tjd=[]
-for i in range(len(mjd)):
-    date=mjd[i]-40000
-    tjd.append(date)
-fname="timestamp_voltage.b1957+20.raw0.node0.scan0"
-man.WriteFile(tjd,"mod{0}.dat".format(fname))
+tjd=mjd-40000 #Convert modified Julian Date to truncated Julian Date
+
+#Create an astropy Time object that includes the coordinates of GMRT
+mjd = Time(mjd, format='mjd', scale='utc', 
+               lon=(74*u.deg+02*u.arcmin+59.07*u.arcsec).to(u.deg).value,
+               lat=(19*u.deg+05*u.arcmin+47.46*u.arcsec).to(u.deg).value)
+
+
+#Greenwich mean sidereal time (in hours)
+gmst=observability.time2gmst(mjd)
+rad_gmst=gmst*np.pi/12
+
+WriteFileCols(tjd,rad_gmst,"mod_{0}.dat".format(fname))
 
