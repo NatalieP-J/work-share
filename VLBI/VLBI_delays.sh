383b20e78058025d8ff9a1fc0f055845752e2bac
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2013-07-31 22:04:44 -0400

    More refinements to VLBI_delays.sh - specifically adding mkdir

--- VLBI/VLBI_delays.sh
+++ VLBI/VLBI_delays.sh
@@ -5,10 +5,10 @@
 #STAMPID='_July26_1810'
 #SOURCE=0
 
-START='Fri Jul 26 22:11:00 UTC 2013'
-END=720
-STAMPID='_July26_1919'
-SOURCE=1
+#START='Fri Jul 26 22:11:00 UTC 2013'
+#END=720
+#STAMPID='_July26_1919'
+#SOURCE=1
 
 #START='Fri Jul 26 22:31:00 UTC 2013'
 #END=600
@@ -25,13 +25,16 @@ SOURCE=1
 #STAMPID='_July27_1919'
 #SOURCE=4
 
-#START='Sat Jul 27 22:26:00 UTC 2013'
-#END=2040
-#STAMPID='_July27_2111'
-#SOURCE=5
+START='Sat Jul 27 22:26:00 UTC 2013'
+END=2040
+STAMPID='_July27_2111'
+SOURCE=5
 
+mkdir TJDtimestamps
+mkdir GSTtimestamps
+mkdir FinalResults
 
 python ~/work-share/VLBI/VLBI_delays.py $SOURCE
-perl ~/work-share/UVWAuto/Timestamp/Convert/convert_timestamp.perl gentimestamp$STAMPID.dat > times$STAMPID.dat
-~/work-share/UVWAuto/tjd2gst/tjd2gst.x <times$STAMPID.dat >GSTtimes$STAMPID.dat
-python ~/work-share/VLBI/uvwconvert.py GSTtimes$STAMPID.dat VLBIdelays$STAMPID.dat $SOURCE
+perl ~/work-share/UVWAuto/Timestamp/Convert/convert_timestamp.perl gentimestamp$STAMPID.dat > TJDtimestamps/times$STAMPID.dat
+~/work-share/UVWAuto/tjd2gst/tjd2gst.x <TJDtimestamps/times$STAMPID.dat >GSTtimestamps/GSTtimes$STAMPID.dat
+python ~/work-share/VLBI/uvwconvert.py GSTtimestamps/GSTtimes$STAMPID.dat FinalResults/VLBIdelays$STAMPID.dat $SOURCE
